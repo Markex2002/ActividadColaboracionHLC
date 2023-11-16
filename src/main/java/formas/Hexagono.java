5ee31aa16d99716e741c3b0a52ecb39be20c4c37
Markex2002 <mmarber462@g.educaand.es>
2023-11-16 10:08:09 +0100

    Hexagono completado

--- src/main/java/formas/Hexagono.java
+++ src/main/java/formas/Hexagono.java
@@ -1,4 +1,36 @@
 package formas;
 
 public class Hexagono {
+    //ATRIBUTOS
+    private double perimetro;
+    private double apotema;
+
+
+    //CONSTRUCTOR
+    public Hexagono(double perimetro, double apotema){
+        this.perimetro = perimetro;
+        this.apotema = apotema;
+    }
+
+    //Constructor en caso de no tener el perimetro
+    public Hexagono(double apotema, double lado1, double lado2, double lado3, double lado4, double lado5, double lado6){
+        perimetro = lado1 + lado2 + lado3 + lado4 + lado5 + lado6;
+        this.apotema = apotema;
+    }
+
+
+    //METODOS
+    public void calcularArea(){
+        double area = (perimetro * apotema) / 2;
+
+        System.out.println("El area de este Hexagono es de: " + area + "cm");
+    }
+
+    @Override
+    public String toString() {
+        String resultado = "";
+        resultado += "Hexagono con perimetro de: " + perimetro + "cm, y apotema de: " + apotema + "cm";
+
+        return resultado;
+    }
 }
