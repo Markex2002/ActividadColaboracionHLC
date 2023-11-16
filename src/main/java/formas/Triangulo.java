7cbcced7e63819772a7bc37dba55d6b5ad101cc1
darioguiles <dario.guilescuesta@hotmail.com>
2023-11-16 09:51:29 +0100

    a

--- src/main/java/formas/Triangulo.java
+++ src/main/java/formas/Triangulo.java
@@ -1,4 +1,12 @@
 package formas;
 
 public class Triangulo {
+    int base;
+    int altura;
+
+    public Triangulo(int base, int altura)
+    {
+        this.base=base;
+        this.altura=altura;
+    }
 }
